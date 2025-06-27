db937ef40cd51eb3161a75cc43b343e59ce74121
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-06-27 04:19:08 +0000

    Update outputs.tf

--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -1,1 +1,11 @@
+# modules/resource_group/outputs.tf
 
+output "resource_group_name" {
+  description = "The name of the created resource group"
+  value       = azurerm_resource_group.this.name
+}
+
+output "resource_group_location" {
+  description = "The location of the created resource group"
+  value       = azurerm_resource_group.this.location
+}
