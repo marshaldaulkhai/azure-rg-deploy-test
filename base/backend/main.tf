bb2fb37de1c007b497658b5b44c5ff772ce2ef53
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-06-27 17:03:55 +0530

    Update main.tf

--- base/backend/main.tf
+++ base/backend/main.tf
@@ -12,7 +12,7 @@ module "storage_account" {
   source                  = "../../modules/storage_account"
   storage_account_name    = var.storage_account_name
   container_name          = var.container_name
-  resource_group_name     = module.resource_group.name
+  resource_group_name     = var.resource_group_name #module.resource_group.name
   location                = var.location
   account_tier            = var.account_tier
   account_replication_type = var.account_replication_type
