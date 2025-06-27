dccb58f3cf83f639851b72d67e18bae0266e3edc
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-06-27 04:17:55 +0000

    Update variables.tf

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -1,1 +1,11 @@
+# modules/resource_group/variables.tf
 
+variable "name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where the resource group will be created"
+  type        = string
+}
