46ef82e16f17e8a52556d9f966899640664f78c4
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-06-27 08:12:37 +0000

    Update outputs.tf

--- base/backend/outputs.tf
+++ base/backend/outputs.tf
@@ -1,6 +1,9 @@
 output "resource_group_name" {
   value = module.resource_group.resource_group_name
 }
+output "resource_group_location" {
+  value = module.resource_group.resource_group_location
+}
 /*
 output "storage_account_name" {
   value = module.storage_account.storage_account_name
