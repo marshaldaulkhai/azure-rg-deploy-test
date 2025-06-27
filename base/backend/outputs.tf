fdba4c53966b7364ec22c6db9d56816fdcfea41a
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-06-27 05:28:32 +0000

    Create outputs.tf

---
+++ base/backend/outputs.tf
@@ -0,0 +1,16 @@
+output "resource_group_name" {
+  value = module.resource_group.name
+}
+
+output "storage_account_name" {
+  value = module.storage_account.storage_account_name
+}
+
+output "container_name" {
+  value = module.storage_account.container_name
+}
+
+output "connection_string" {
+  value     = module.storage_account.primary_connection_string
+  sensitive = true
+}
