37884bdfaa3bda3521c5eb79a5f6556dfea2ee26
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-06-27 05:23:54 +0000

    Create main.tf

---
+++ modules/storage_account/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_storage_account" "this" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+
+  allow_blob_public_access = false
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_container" "this" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.this.name
+  container_access_type = "private"
+}
