084f954189128d62f00cc46daf98fc7545b881bf
Marshal Kumar <marshaldaulkhai@gmail.com>
2025-07-01 13:40:11 +0530

    This commit will trigger a pipleine to deploy a RG.

--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -2,6 +2,7 @@ output "name"{
   description = ""
   value       = azurerm_resource_group.main.name
 }
+
 output "location"{
   description = ""
   value       = azurerm_resource_group.main.location
