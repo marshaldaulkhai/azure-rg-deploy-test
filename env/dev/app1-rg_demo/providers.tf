9f3f9459d2e59e9e934fdf98da8a3eae6fd9392a
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-07-04 14:55:17 +0530

    Update providers.tf

--- env/dev/app1-rg_demo/providers.tf
+++ env/dev/app1-rg_demo/providers.tf
@@ -1,15 +1,26 @@
+# env/dev/app1-rg_demo/providers.tf
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0" 
+      version = "~> 3.0"
     }
   }
-  required_version = ">= 1.0.0" 
+  required_version = ">= 1.0.0"
 }
 
-# Configure the AzureRM Provider
+# Configure the default AzureRM Provider for resources in East US
 provider "azurerm" {
-  features {} 
+  features {}
+  subscription_id = var.subscription_id # Get subscription ID from variable
+  location        = "East US"           # Set the default region for this provider instance
 }
-    
+
+# Configure an aliased AzureRM Provider for resources in West US
+provider "azurerm" {
+  alias           = "westus"            # Give this provider configuration an alias
+  features {}
+  subscription_id = var.subscription_id # Use the same subscription ID
+  location        = "West US"           # Set the specific region for the aliased provider
+}
