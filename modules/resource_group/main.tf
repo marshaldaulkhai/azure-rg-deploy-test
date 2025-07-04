45168e08d65e3baa03b0683bf1d3dfb282e9444d
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-07-04 15:09:29 +0530

    Update main.tf

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,4 +1,17 @@
+# modules/resource_group/main.tf
+
+# Add this block at the top of your module's main.tf file
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      # You can optionally specify a version constraint here if your module
+      # has specific compatibility requirements, e.g., version = "~> 3.0"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "main" {
-  name      =  var.name
-  location  =  var.location
+  name     = var.name
+  location = var.location
 }
