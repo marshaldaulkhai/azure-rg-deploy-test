949532be68b7b84e204a94e44a46b877a2649d59
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-07-29 16:32:00 +0530

    Update variable.tf

--- env/dev/app1-rg_demo/variable.tf
+++ env/dev/app1-rg_demo/variable.tf
@@ -13,3 +13,21 @@ variable "web_app_name" {
   description = "The name for the web app"
   type        = string
 }
+
+variable "main_client_id" {
+  description = "The client ID for the main service principal"
+  type        = string
+}
+
+variable "main_client_secret" {
+  description = "The client secret for the main service principal"
+  type        = string
+  sensitive   = true
+}
+
+variable "main_tenant_id" {
+  description = "The tenant ID for the main Azure tenant"
+  type        = string
+}
+
+
