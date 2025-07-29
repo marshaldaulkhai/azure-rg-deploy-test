4ed77a8d862c0287e9d5a72b7b90a8ace210e370
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-07-29 14:32:27 +0530

    Update main.tf

--- env/dev/app1-rg_demo/main.tf
+++ env/dev/app1-rg_demo/main.tf
@@ -17,26 +17,20 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_service_plan" "app_service_plan" {
   name                = "example-appservice-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-
-  # For Linux web apps, uncomment the next two lines:
-  # kind     = "Linux"
-  # reserved = true
+  sku_name            = "S1"
+  os_type             = "Windows" # Or "Linux"
 }
 
 resource "azurerm_app_service" "app_service" {
   name                = "example-webapp-12345"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  app_service_plan_id = azurerm_service_plan.app_service_plan.id
 
   site_config {
     dotnet_framework_version = "v4.0"     # For .NET apps
