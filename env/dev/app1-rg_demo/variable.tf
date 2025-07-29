c830ebc849964658d5f675401e17760448d48ead
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-07-29 16:16:56 +0530

    Update variable.tf

--- env/dev/app1-rg_demo/variable.tf
+++ env/dev/app1-rg_demo/variable.tf
@@ -1,50 +1,15 @@
-# --- Credentials for the main (default) Service Principal 
-resource_group_name = "example-app-rg"
-location            = "East US"
-app_service_plan_id = "/subscriptions/6d41859f-600b-4bdf-9b5d-084a0582cb54/resourceGroups/example-app-rg/providers/Microsoft.Web/serverfarms/example-appservice-plan"
-web_app_name        = "example-webapp-12345"
-
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
 
 variable "location" {
-  description = "The Azure location to deploy resources"
+  description = "The Azure location"
   type        = string
-  default     = "East US"   # optional; remove default if you want it mandatory
+  default     = "East US" # optional default value
 }
 
 variable "web_app_name" {
-  description = "The name of the web app (must be globally unique)"
+  description = "The name for the web app"
   type        = string
 }
-
-variable "audit_tenant_id" {
-  description = "Tenant ID for audit purposes"
-  type        = string
-}
-
-variable "main_client_id" {
-  description = "Client ID for main app"
-  type        = string
-}
-
-variable "main_client_secret" {
-  description = "Client secret for main app"
-  type        = string
-  sensitive   = true
-}
-
-variable "main_tenant_id" {
-  description = "Tenant ID for the main app"
-  type        = string
-}
-
-variable "audit_client_id" {
-  description = "Audit client ID"
-  type        = string
-}
-
-variable "audit_client_secret" {
-  description = "Audit client secret"
-  type        = string
-  sensitive   = true
-}
-
