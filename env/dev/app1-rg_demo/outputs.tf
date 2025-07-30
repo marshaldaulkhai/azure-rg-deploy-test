3ad47e419ea481d00d2671c24a263cfb2bd4acc9
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-07-30 09:44:41 +0530

    Create outputs.tf

---
+++ env/dev/app1-rg_demo/outputs.tf
@@ -0,0 +1,19 @@
+output "web_app_default_hostname" {
+  value = var.app_service_plan_os_type == "Linux" ? azurerm_linux_web_app.linux_webapp[0].default_site_hostname : azurerm_windows_web_app.windows_webapp[0].default_site_hostname
+}
+
+output "app_service_plan_id" {
+  value = azurerm_app_service_plan.app_service_plan.id
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "private_endpoint_id" {
+  value = var.enable_private_endpoint ? azurerm_private_endpoint.webapp_pe[0].id : ""
+}
+
+output "diagnostic_setting_id" {
+  value = (var.enable_monitoring && length(var.log_analytics_workspace_id) > 0) ? azurerm_monitor_diagnostic_setting.webapp_diag[0].id : ""
+}
