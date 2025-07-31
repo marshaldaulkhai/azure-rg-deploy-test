e2bf9bee9842b14e67e2148e2bac54e6fd8612ce
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-07-31 10:50:35 +0530

    Update versions.tf

--- env/dev/app1-rg_demo/versions.tf
+++ env/dev/app1-rg_demo/versions.tf
@@ -1,5 +1,6 @@
 terraform {
-  required_version = ">= 1.8.5"
+  #required_version = ">= 1.8.5"
+  required_version = ">= 1.5.7"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
