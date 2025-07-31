6911f9792939aa5b00adadb911b76b7907adf10d
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-07-31 13:06:24 +0530

    Update locals.tf

--- env/dev/app1-rg_demo/locals.tf
+++ env/dev/app1-rg_demo/locals.tf
@@ -15,5 +15,5 @@ locals {
   short_location = local.locations[var.location]
   resource_group_name = "rg-${lower(var.product)}-${lower(var.application_name)}-${local.web_app_env[local.env]}"
   web_app_name   = "dte-app-${local.short_location}-${local.web_app_env[local.env]}-${lower(var.application_name)}"
-  app_service_plan_name   = "asp-${local.short_location}-${local.web_app_env[local.env]}-${lower(var.application_name)}${lower.os}"
+  app_service_plan_name   = "asp-${local.short_location}-${local.web_app_env[local.env]}-${lower(var.application_name)}${local.os}"
 }
