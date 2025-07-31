6d389b2d38a7585ffc7641caa076b6f5c0f3e264
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-07-31 12:20:33 +0530

    Update locals.tf

--- env/dev/app1-rg_demo/locals.tf
+++ env/dev/app1-rg_demo/locals.tf
@@ -1,5 +1,6 @@
 locals {
   env = lower(var.environment)
+  os = lower(var.app_service_plan_os_type)
   locations = {
     centralus = "cu"
     eastus2   = "e2"
@@ -13,4 +14,5 @@ locals {
   }
   short_location = local.locations[var.location]
   web_app_name   = "dte-app-${local.short_location}-${local.web_app_env[local.env]}-${lower(var.application_name)}"
+  app_service_plan_name   = "asp-${local.short_location}-${local.web_app_env[local.env]}-${lower(var.application_name)}${lower.os}"
 }
